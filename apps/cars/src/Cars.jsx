68fcac4a8605c368aceb3d32d3a0602ec4bca379
vintenj <joe.vinten@vodafone.com>
2022-09-12 09:16:56 +0100

    update the car labels

--- apps/cars/src/Cars.jsx
+++ apps/cars/src/Cars.jsx
@@ -1,14 +1,45 @@
+import { useState } from "react"
 import Button from "@obelix/button"
+import { TextInput } from "@obelix/text-input"
 
 /*
   Make, Colour, Year, Doors, Reg
 */
 
 const Cars = () => {
+	const [cars, setCars] = useState({
+		make: "",
+		colour: "",
+		doors: "",
+		reg: "",
+		year: "",
+	})
+
+	const handleChange = (e) => {
+		setCars((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
+	}
+
+	const carsArray = ["make", "colour", "year", "doors", "reg"]
+
 	return (
 		<div>
-			<div>Form goes here</div>
-			<Button onClick={() => {}}>Submit form</Button>
+			<div>
+				<form>
+					{carsArray.map((item) => {
+						const labelTransformed = item[0].toUpperCase() + item.slice(1)
+						return <TextInput label={labelTransformed} key={item} name={item} onChange={handleChange} value={cars[item]} />
+					})}
+				</form>
+			</div>
+
+			<Button
+				onClick={(e) => {
+					e.preventDefault()
+					console.log(cars)
+				}}
+			>
+				Submit form
+			</Button>
 		</div>
 	)
 }
--- packages/TextInput/TextInput.jsx
+++ packages/TextInput/TextInput.jsx
@@ -1,5 +1,12 @@
 import React from "react"
 
-export const TextInput = ({ children, ...props }) => <input type='text' {...props} />
+export const TextInput = ({ children, ...props }) => {
+	return (
+		<label>
+			{props.label}
+			<input type='text' {...props} />
+		</label>
+	)
+}
 
 export default TextInput
